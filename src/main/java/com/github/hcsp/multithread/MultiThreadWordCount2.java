c5fb4b109f1728df8c3a43b927bd9f4be34ea86c
Yuanchen Wang <yuanchen.wang@optus.com.au>
2023-09-11 13:09:15 +1000

    added code for using con-currenthashmap

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount2.java
@@ -1,8 +1,55 @@
 package com.github.hcsp.multithread;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.*;
+
+
 public class MultiThreadWordCount2 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+    private static ConcurrentHashMap<String, Integer> resMap = new ConcurrentHashMap<>();
+    public static Map<String, Integer> count(int threadNum, List<File> files) {
+        for(int i =0;i<threadNum;i++){
+            final int index_rubbish = i;
+            new Thread(() -> {
+                //todo: each new thread know which file they are going to open
+                //then
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(files.get(index_rubbish)));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        //todo: what we are getting here
+                        //string manipulation here
+
+                        String[] str_line = line.split(" ");//use the white space as delimeter
+                        for(String afterSplitStr : str_line){
+                            //todo: check if afterSplitstr is inside the dict
+                            if(resMap.containsKey(afterSplitStr)){
+                                resMap.put(afterSplitStr,resMap.get(afterSplitStr)+1);
+                            }else{
+                                resMap.put(afterSplitStr, 1);
+                            }
+                        }
+
+
+                        //todo: update the concurrent hashmap right here
+
+                    }
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }).start();
+        }
+            HashMap<String, Integer> hashMap = new HashMap<>(resMap);
+            return hashMap;
+    }
+
+
 }
--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount3.java
@@ -1,8 +1,43 @@
 package com.github.hcsp.multithread;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MultiThreadWordCount3 {
-    // 使用threadNum个线程，并发统计文件中各单词的数量
-    //    public static Map<String, Integer> count(int threadNum, List<File> files) {
-    //        return null;
-    //    }
+//    // 使用threadNum个线程，并发统计文件中各单词的数量
+//    AtomicInteger atomicInt = new AtomicInteger(0);
+//    public static Map<String, Integer> count(int threadNum, List<File> files) {
+////        return null;
+//        List<Integer> l1 = new ArrayList<>();
+//        return null;
+//    }//pointer
+//
+//
+//    public static Map<String, Integer> map = new HashMap<>();// class var
+//
+//    public Map<String, Integer> map2 = new HashMap<>();// instance
+//    private static void bar(int a){
+//        System.out.println(a);
+//
+//    }
+//    public static void main(String[] args) {
+////        bar(3);
+//        //sync
+//        bar(3);
+//        new Thread(() -> {
+//            System.out.println("Thread is running using lambda.");
+//            bar(5);
+//        }).start();
+////        try {
+////            Thread.sleep(5000);
+////        } catch (InterruptedException e) {
+////            throw new RuntimeException(e);
+////        }
+//
+//    }
+
 }
